d4a55bb37f19ba10c26de47766f490a3b1eeb067
AlexanderCholiy <alexander.choliy@gmail.com>
2025-07-11 15:36:46 +0000

    Добавили шаблоны формы

--- open5gs/views.py
+++ open5gs/views.py
@@ -1,7 +1,7 @@
-from typing import Optional
+from typing import Optional, Union
 
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 from users.utils import role_required
@@ -22,29 +22,30 @@ def index(request: HttpRequest) -> HttpResponse:
 @role_required()
 def subscriber(
     request: HttpRequest, imsi: Optional[str] = None
-) -> HttpResponse:
+) -> Union[HttpResponse, HttpResponseRedirect]:
     template_name = 'open5gs/subscriber_form.html'
+    instance = get_object_or_404(Subscriber, imsi=imsi) if imsi else None
 
-    if imsi is not None:
-        instance = get_object_or_404(Subscriber, imsi=imsi)
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect('open5gs:index')
     else:
-        instance = None
+        form = SubscriberForm(instance=instance)
 
-    form = SubscriberForm(request.POST or None, instance=instance)
     context = {'form': form}
-    if form.is_valid():
-        form.save()
-
     return render(request, template_name, context)
 
 
 @login_required
 @role_required()
-def delete_subscriber(request: HttpRequest, imsi: str):
+def delete_subscriber(
+    request: HttpRequest, imsi: str
+) -> Union[HttpResponse, HttpResponseRedirect]:
     instance = get_object_or_404(Subscriber, imsi=imsi)
-    form = SubscriberForm(instance=instance)
-    context = {'form': form}
     if request.method == 'POST':
         instance.delete()
         return redirect('open5gs:index')
-    return render(request, 'open5gs/subscriber_form.html', context)
+    context = {'subscriber': instance}
+    return render(request, 'open5gs/subscriber_delete.html', context)
