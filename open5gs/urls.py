7091dd4ea9cd808f23a43780f50309b7d596c4fd
AlexanderCholiy <alexander.choliy@gmail.com>
2025-07-14 10:44:42 +0000

    Добавили css для main

--- open5gs/urls.py
+++ open5gs/urls.py
@@ -8,8 +8,8 @@ app_name = 'open5gs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('subscriber/', views.subscriber, name='create'),
-    path('subscriber/<str:imsi>/edit/', views.subscriber, name='edit'),
+    path('subscriber/<int:imsi>/edit/', views.subscriber, name='edit'),
     path(
-        'subscriber/<str:imsi>/delete/', views.delete_subscriber, name='delete'
+        'subscriber/<int:imsi>/delete/', views.delete_subscriber, name='delete'
     ),
 ]
--- open5gs/views.py
+++ open5gs/views.py
@@ -1,32 +1,53 @@
 from typing import Optional, Union
-from random import randint
+from urllib.parse import urlencode
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from users.utils import role_required
 from .forms import SubscriberForm
 from .models import Subscriber
+from .constants import MAX_SUBSCRIBER_PER_PAGE
 
 
 @login_required
 @role_required()
 def index(request: HttpRequest) -> HttpResponse:
     template_name = 'open5gs/index.html'
-    # subscribers = Subscriber.objects.values('pk', 'imsi').order_by('-pk')
-    subscribers = [str(randint(i, 10**5)) for i in range(10)]
-    context = {'subscribers': subscribers}
+
+    query = request.GET.get('q', '').strip()
+    if query:
+        subscribers = (
+            Subscriber.objects.values('pk', 'imsi')
+            .filter(imsi__icontains=query).order_by('-pk')
+        )
+    else:
+        subscribers = Subscriber.objects.values('pk', 'imsi').order_by('-pk')
+
+    paginator = Paginator(subscribers, MAX_SUBSCRIBER_PER_PAGE)
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+    query_params = request.GET.copy()
+    if 'page' in query_params:
+        del query_params['page']
+    page_url_base = '?'
+    if query_params:
+        page_url_base += urlencode(query_params) + '&'
+
+    context = {'page_obj': page_obj, 'search_query': query, 'page_url_base': page_url_base}
     return render(request, template_name, context)
 
 
 @login_required
 @role_required()
 def subscriber(
-    request: HttpRequest, imsi: Optional[str] = None
+    request: HttpRequest, imsi: Optional[int] = None
 ) -> Union[HttpResponse, HttpResponseRedirect]:
     template_name = 'open5gs/subscriber_form.html'
-    instance = get_object_or_404(Subscriber, imsi=imsi) if imsi else None
+    instance = get_object_or_404(Subscriber, imsi=str(imsi)) if imsi else None
 
     if request.method == 'POST':
         form = SubscriberForm(request.POST, instance=instance)
@@ -43,9 +64,9 @@ def subscriber(
 @login_required
 @role_required()
 def delete_subscriber(
-    request: HttpRequest, imsi: str
+    request: HttpRequest, imsi: int
 ) -> Union[HttpResponse, HttpResponseRedirect]:
-    instance = get_object_or_404(Subscriber, imsi=imsi)
+    instance = get_object_or_404(Subscriber, imsi=str(imsi))
     if request.method == 'POST':
         instance.delete()
         return redirect('open5gs:index')
