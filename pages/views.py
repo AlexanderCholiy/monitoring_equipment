722e38b2f9acdfa5886cf8181b1f2b6712f75196
lexander holiy <alexander.choliy@gmail.com>
2025-07-17 22:49:47 +0300

    стили для страниц ошибок

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,8 @@ from django.views.generic import TemplateView
 
 
 class AboutTemplateView(TemplateView):
-    template_name = 'pages/about.html'
+    # template_name = 'pages/about.html'
+    template_name = 'core/404.html'
 
     def get_context_data(self: 'AboutTemplateView', **kwargs: dict) -> dict:
         context = super().get_context_data(**kwargs)
