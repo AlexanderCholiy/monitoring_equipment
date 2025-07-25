c4e6bb70cda059f86c1c40152f37c01c7bbc6717
lexander holiy <alexander.choliy@gmail.com>
2025-07-25 18:51:54 +0300

    Добавили managment команду для пользователя по умолчанию

--- users/management/commands/add_default_admin.py
+++ users/management/commands/add_default_admin.py
@@ -2,51 +2,59 @@ import os
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-
-from users.models import PendingUser
+from django.db.models import Q
 from dotenv import load_dotenv
 
+from users.models import PendingUser
 
 load_dotenv(override=True)
 
 
 class Command(BaseCommand):
     help = 'Создает дефолтного администратора, если он не существует'
-    ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', 'admin')
-    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'admin@newtowers.ru')
-    ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', '3uGMw69J6fflgFsSpA76ebSuk')
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
 
+        ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', 'admin')
+        ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'admin@newtowers.ru')
+        ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', '3uGMw69J6fflSpA76ebSuk')
+
         if User.objects.filter(
             username=ADMIN_USERNAME, is_superuser=True
         ).exists():
-            print(f'✅ Пользователь "{ADMIN_USERNAME}" уже существует.')
+            self.stdout.write(
+                f'✅ Администратор "{ADMIN_USERNAME}" уже существует.'
+            )
             return
 
-        # Удаляем PendingUser с таким же username или email, если есть
-        deleted, _ = PendingUser.objects.filter(
-            username=ADMIN_USERNAME
+        pending_deleted, _ = PendingUser.objects.filter(
+            Q(username=ADMIN_USERNAME) | Q(email=ADMIN_EMAIL)
         ).delete()
-        if deleted:
-            self.stdout.write(f"🧹 Удалён PendingUser с username='{ADMIN_USERNAME}'")
+        if pending_deleted:
+            self.stdout.write(
+                f'🧹 Удалены PendingUser с username="{ADMIN_USERNAME}" '
+                f'или email="{ADMIN_EMAIL}"'
+            )
 
-        deleted, _ = PendingUser.objects.filter(
-            email=ADMIN_EMAIL
-        ).delete()
-        if deleted:
-            self.stdout.write(f"🧹 Удалён PendingUser с email='{ADMIN_EMAIL}'")
+        user_deleted, _ = User.objects.filter(
+            Q(username=ADMIN_USERNAME) | Q(email=ADMIN_EMAIL),
+        ).exclude(is_superuser=True).delete()
+        if user_deleted:
+            self.stdout.write(
+                '🧹 Удалены обычные пользователи с '
+                f'username="{ADMIN_USERNAME}" или email="{ADMIN_EMAIL}"'
+            )
 
-        # Создаем пользователя
-        user = User.objects.create_user(
+        User.objects.create_user(
             username=ADMIN_USERNAME,
             email=ADMIN_EMAIL,
             password=ADMIN_PASSWORD,
-            role='user'  # или 'admin', если добавишь такую роль
+            role='user',
+            is_staff=True,
+            is_superuser=True,
         )
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
 
-        self.stdout.write(self.style.SUCCESS(f"✅ Администратор '{ADMIN_USERNAME}' успешно создан."))
+        self.stdout.write(
+            f'✅ Администратор "{ADMIN_USERNAME}" успешно создан.'
+        )
